b48160d5bfd9279cf14015765c1f803a49645dbc
Yusuke Hatanaka <hatanaka@cloverlab.jp>
2014-04-03 20:58:24 +0900

    add .tigrc

--- install.sh
+++ install.sh
@@ -4,7 +4,9 @@ rm ~/.tmux.conf
 rm ~/.zshrc
 rm ~/.vimrc
 rm ~/.weechat
+rm ~/.tigrc
 ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
 ln -s ~/.dotfiles/.weechat ~/.weechat
+ln -s ~/.dotfiles/.tigrc ~/.tigrc
