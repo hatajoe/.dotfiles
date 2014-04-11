ac9a3c254edd9e94118a25511eb4dcb710a68552
Yusuke Hatanaka <arbalestimp@gmail.com>
2014-04-11 11:43:26 +0900

    gitconfig

--- install.sh
+++ install.sh
@@ -4,10 +4,14 @@ rm ~/.tmux.conf
 rm ~/.zshrc
 rm ~/.vimrc
 rm -rf ~/.weechat
+rm -rf ~/.composer
 rm ~/.tigrc
+rm ~/.gitconfig
 ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
 ln -s ~/.dotfiles/.weechat ~/.weechat
+ln -s ~/.dotfiles/.composer~/.composer
 ln -s ~/.dotfiles/.tigrc ~/.tigrc
+ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 
