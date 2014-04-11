650d88b24350ce72370acf490a12482f3a5ee4af
Yusuke Hatanaka <arbalestimp@gmail.com>
2014-04-11 11:46:50 +0900

    bag fix

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
 ln -s ~/.dotfiles/.weechat ~/.weechat
-ln -s ~/.dotfiles/.composer~/.composer
+ln -s ~/.dotfiles/.composer ~/.composer
 ln -s ~/.dotfiles/.tigrc ~/.tigrc
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 
