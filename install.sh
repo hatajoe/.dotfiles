6cb8b2330c561169099d96a1e54098ba5be90885
Yusuke Hatanaka <arbalestimp@gmail.com>
2014-07-05 14:43:44 +0900

    ファイル削除処理を削除

--- install.sh
+++ install.sh
@@ -1,22 +1,8 @@
 #!/bin/sh
-
-rm ~/.bash_profile
-rm ~/.tmux.conf
-rm ~/.zshrc
-rm ~/.vimrc
-rm -rf ~/.weechat
-rm -rf ~/.composer
-rm -rf ~/.git_template
-rm ~/.tigrc
-rm ~/.gitconfig
-rm ~/.git-completion.bash
-rm ~/.git-prompt.sh
-rm -rf ~/.config/powerline
-rm -rf ~/.percol.d
-
 ln -s ~/.dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
+ln -s ~/.dotfiles/.zshenv ~/.zshenv
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
 ln -s ~/.dotfiles/.weechat ~/.weechat
 ln -s ~/.dotfiles/.composer ~/.composer
@@ -26,5 +12,3 @@ ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/.git-completion.bash ~/.git-completion.bash
 ln -s ~/.dotfiles/.git-prompt.sh ~/.git-prompt.sh
 ln -s ~/.dotfiles/.config/powerline ~/.config/powerline
-ln -s ~/.dotfiles/.percol.d ~/.percol.d
-
