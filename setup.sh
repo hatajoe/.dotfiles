e7341174205471f4745bbfa8001ad258698ccfee
Yusuke Hatanaka <arbalestimp@gmail.com>
2019-11-28 01:21:55 +0900

    update

--- setup.sh
+++ setup.sh
@@ -22,6 +22,8 @@ brew install tig
 brew install hub
 brew install vim
 brew install tmux
+brew install tmuxinator
+brew install direnv
 brew install anyenv
 brew install fzf
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
@@ -33,3 +35,9 @@ ln -s -f $PWD/.tmux.conf $HOME/.tmux.conf
 ln -s -f $PWD/.vimrc $HOME/.vimrc
 ln -s -f $PWD/.zshenv $HOME/.zshenv
 ln -s -f $PWD/.zshrc $HOME/.zshrc
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+mkdir -p ~/.vim/directory
+mkdir -p ~/.vim/backupdir
+mkdir -p ~/.vim/undodir
+#
